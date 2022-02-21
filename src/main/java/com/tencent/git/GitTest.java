0039f7b6a809115cb2c1df7699b60dd05dee3cb2
shy <2497141901@qq.com>
2022-02-21 13:34:27 +0800

    first commit

---
+++ src/main/java/com/tencent/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.tencent.git;
+
+/**
+ * @author shy
+ * @create 2022-02-21 13:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
